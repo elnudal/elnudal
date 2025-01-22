b23803833561ee7f492eb4943a407d0a36537f8a
Lisandro <lisandropat@gmail.com>
2025-01-23 00:09:24 +0100

    Linktree

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,38 @@
 import * as React from "react"
 import SEO from "../components/seo"
 import "../styles/index.css"
+import Logo from "../styles/images/LogoOrangeVolume.svg"
 
 const IndexPage = () => {
   return (
-    <main>
-      <h1>This is the home page.</h1>
-    </main>
+    <>
+      <div id="linktree">
+        <div className="texture" />
+        <main>
+          <img src={Logo} alt="El Nudal Macrame y Decoracion Fina" />
+          <section class="links">
+            <a href="#" target="_blank" rel="noreferrer">
+              <button>
+                <i class="ph ph-shopping-cart"></i> Tienda Online <span className="badge">Nuevo</span>
+              </button>
+            </a>
+            <a href="https://instagram.com/elnudal" target="_blank" rel="noreferrer">
+              <button>
+                <i class="ph ph-instagram-logo"></i> Instagram
+              </button>
+            </a>
+            <a href="#" target="_blank" rel="noreferrer">
+              <button>
+                <i class="ph ph-facebook-logo"></i> Facebook
+              </button>
+            </a>
+          </section>
+          <a href="https://instagram.com/elnudal" target="_blank" rel="noreferrer" className="footer-link">
+            <span>Ó contactanos por What's App</span> <i class="ph ph-whatsapp-logo"></i>
+          </a>
+        </main>
+      </div>
+    </>
   )
 }
 
